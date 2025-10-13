2887156875e777c7ffafad465f47ba8d1e59e96b
cesarve-dev <cesar.radiate@gmail.com>
2025-10-13 00:08:18 -0700

    fixed issues suggested by PR reviewer.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-async function getUser() {
-    const response = await fetch("https://api.github.com/users/cesarve-dev/repos");
+async function getRepositories() {
     try {
+        const response = await fetch("https://api.github.com/users/cesarve-dev/repos");
+        
         if (!response.ok) {
             throw new Error(response.status);
         } 
@@ -15,4 +16,48 @@ async function getUser() {
     }    
 }
 
-getUser();
+getRepositories();
+
+//Soccer API
+
+//This returns Peru National Team information
+// fetch("https://v3.football.api-sports.io/teams?id=30", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "v3.football.api-sports.io",
+// 		"x-rapidapi-key": "ee2671c057388eed73c0832729ea0572"
+// 	}
+// })
+// .then(response => {
+// 	returnresponse.json();
+// })
+// .then(data => {
+//     console.log(data);
+// })
+// .catch(err => {
+// 	console.log(err);
+// });
+
+//This returns a list of players from The Peruvian National Team
+// fetch("https://v3.football.api-sports.io/players/squads?team=30", {
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-host": "v3.football.api-sports.io",
+// 		"x-rapidapi-key": "ee2671c057388eed73c0832729ea0572"
+// 	}
+// })
+// .then(response => {
+//     return response.json();
+// })
+// .then(data => {
+//     console.log(data)
+// })
+// .catch(err => {
+//     console.log(err);
+// })
+
+/* 
+Next Steps:
+1. think about how do you want to display the information
+2. Add css for styling
+*/
