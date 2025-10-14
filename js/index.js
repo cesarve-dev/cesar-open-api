e569459d67dba2f5141edb95d69fdac378f57a23
cesarve-dev <cesar.radiate@gmail.com>
2025-10-14 03:23:17 -0700

    display data points.

--- js/index.js
+++ js/index.js
@@ -20,8 +20,13 @@ getRepositories();
 
 //Soccer API
 
+//sections
+const teamInfo = document.getElementById('team-info');
+const teamStadium = document.getElementById('team-stadium');
+const playerCardContainer= document.querySelector('.player-card-container');
+
 //This returns Peru National Team information
-async function getPeruviaTeamInfo() {
+async function getPeruvianTeamInfo() {
     try {
         const response = await fetch("https://v3.football.api-sports.io/teams?id=30", {
             "method": "GET",
@@ -36,13 +41,59 @@ async function getPeruviaTeamInfo() {
         }
 
         const data = await response.json();
-        console.log(data.response[0].team.name); //Peru
+
+        //data points    
+        const title = data.response[0].team.name;
+        const country = data.response[0].team.country;
+        const founded = data.response[0].team.founded;
+        const logo = data.response[0].team.logo;
+        const stadiumName = data.response[0].venue.name;
+        const stadiumAddress = data.response[0].venue.address;
+        const stadiumCity = data.response[0].venue.city;
+        const stadiumCapacity = data.response[0].venue.capacity;
+        const stadiumSurface = data.response[0].venue.surface;
+        const stadiumImage = data.response[0].venue.image;
+        
+        //content for #team-info section
+        const newH2 = document.createElement('h2');
+        const newPcountry = document.createElement('p');
+        const newPcity = document.createElement('p');
+        const newPfounded = document.createElement('p');
+        const imageLogo = document.createElement('img');
+        newH2.innerHTML = `National soccer team of: ${title}`;
+        newPcity.innerHTML = `Located in the city of: ${stadiumCity}`;
+        newPcountry.innerHTML = `and country of: ${country}`;
+        newPfounded.innerHTML = `The Peru National Soccer Team was founded on: ${founded}`;
+        imageLogo.src = `${logo}`;
+
+        teamInfo.appendChild(newH2);
+        teamInfo.appendChild(newPcity);
+        teamInfo.appendChild(newPcountry);
+        teamInfo.appendChild(newPfounded);
+        teamInfo.appendChild(imageLogo);
+
+        //content for #team-stadium section
+        const stadiumPname = document.createElement('p');
+        const stadiumPaddress = document.createElement('p');
+        const stadiumPcapacity = document.createElement('p');
+        const stadiumPsurface = document.createElement('p');
+        const stadiumImg = document.createElement('img');
+        stadiumPname.innerHTML = `Name: ${stadiumName}`;
+        stadiumPaddress.innerHTML = `Address: ${stadiumAddress}, ${stadiumCity}`;
+        stadiumPcapacity.innerHTML = `The stadium has a capacity for ${stadiumCapacity} seats`;
+        stadiumPsurface.innerHTML = `Type of surface: ${stadiumSurface}`;
+        stadiumImg.src = `${stadiumImage}`;
+
+        teamStadium.appendChild(stadiumPname);
+        teamStadium.appendChild(stadiumPaddress);
+        teamStadium.appendChild(stadiumPcapacity);
+        teamStadium.appendChild(stadiumPsurface);
+        teamStadium.appendChild(stadiumImg);
 
     } catch (error) {
         console.log('new error:', error);
     }
 }
-
 
 //This returns a list of players from The Peruvian National Team
 async function getPlayersInfo () {
@@ -60,10 +111,39 @@ async function getPlayersInfo () {
         }
 
         const data = await response.json();
-        console.log(data.response[0].players.length);
+
+        // Content for #team-players section
+        
 
         for (let i = 0; i < data.response[0].players.length; i++) {
-            console.log(data.response[0].players[i].name) //names of each player
+            //data points
+            const dataPlayerName = data.response[0].players[i].name;
+            const dataPlayerAge = data.response[0].players[i].age;
+            const dataPlayerNumber = data.response[0].players[i].number;
+            const dataPlayerPosition = data.response[0].players[i].position;
+            const dataPlayerPhoto = data.response[0].players[i].photo;
+
+            //content for .playerCard
+            const playerName = document.createElement('h3');
+            const playerAge = document.createElement('p');
+            const playerNumber = document.createElement('p');
+            const playerPosition = document.createElement('p');
+            const playerPhoto = document.createElement('img');
+            const playerCard = document.createElement('div');
+            playerCard.classList.add('player-card');
+            playerName.innerHTML = `${dataPlayerName}`;
+            playerAge.innerHTML = `Age: ${dataPlayerAge}`;
+            playerNumber.innerHTML = `Jersey number: ${dataPlayerNumber}`;
+            playerPosition.innerHTML = `Position: ${dataPlayerPosition}`;
+            playerPhoto.src = `${dataPlayerPhoto}`;
+
+            playerCard.append(playerPhoto);
+            playerCard.append(playerName);
+            playerCard.append(playerAge);
+            playerCard.append(playerNumber);
+            playerCard.append(playerPosition);
+
+            playerCardContainer.append(playerCard);
         }
 
     } catch (error) {
@@ -72,10 +152,4 @@ async function getPlayersInfo () {
 }
 
 getPlayersInfo();
-getPeruviaTeamInfo();
-
-/* 
-Next Steps:
-1. think about how do you want to display the information
-2. Add css for styling
-*/
+getPeruvianTeamInfo();
